7f323a986d52fc7bb16438d43dc1882275d0f155
perepichai <o.perepichai@gmail.com>
2022-04-20 10:23:22 +0300

    feat: add main component

--- currency-converter/src/app/shell/main/main.component.ts
+++ currency-converter/src/app/shell/main/main.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Select } from '@ngxs/store';
+import { Observable, Subject, takeUntil } from 'rxjs';
+import { Currency } from 'src/app/models/currencies.model';
+import { GetCurrencies } from 'src/app/store/currencies.actions';
+import { CurrenciesState } from 'src/app/store/currencies.state';
 
 @Component({
   selector: 'app-main',
@@ -6,10 +12,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
+  @Select(CurrenciesState.currencies)
+  currencies$!: Observable<Currency[]>;
+  currencies: Currency[] = [];
+  private destroy$: Subject<boolean> = new Subject<boolean>();
 
-  constructor() { }
+  constructor( private store: Store) { }
 
   ngOnInit(): void {
+    this.currencies$.pipe(
+      takeUntil(this.destroy$),
+    ).subscribe(currencies => this.currencies = currencies);
+    this.store.dispatch(new GetCurrencies());
   }
 
 }
